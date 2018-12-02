a22d194354b46448d61369ef2db51a3c76baf6d1
Cam Van <hocamvan.htcv@gmail.com>
2018-12-02 22:34:06 +0100

    first commit

---
+++ src/routes/clubs.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const connection = require('../helper/db.js')
+const Router = express.Router();
+
+Router.get ('/', (req,res) => {
+    connection.query('SELECT * from club', (err, results) => {
+        if (err) {
+            res.status(500).send('Erreur lors de la récupération des employés');
+        } else {
+            res.json(results);
+        }
+    });
+})
+
+module.exports = Router;
---
+++ src/routes/genericCrud.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const connection = require('../helper/conf')
+const router = express.Router();
+const tables = ['club', 'order'];
+const isTableNotAuthorized = (table) => {
+    return !tables.includes(table.toLowerCase());
+};
+router.get('/:table', (req, res) => {
+    if (isTableNotAuthorized(req.params.table)) {
+        res.send(400);
+        return;
+    }
+    connection.query('SELECT * from ' + req.params.table, (err, results) => {
+        if (err) {
+            res.status(500).send('Erreur lors de la récupération des' + req.params.table);
+        } else {
+            res.json(results);
+        }
+    });
+})
+
+router.get('/:table/:id', (req, res) => {
+    if (isTableNotAuthorized(req.params.table)) {
+        res.send(400);
+        return;
+    }
+    console.log({ r: req.params });
+    connection.query('SELECT * from ' + req.params.table + ' where id=?', req.params.id, (err, results) => {
+        if (err) {
+            res.status(500).send('Erreur lors de la récupération des' + req.params.table);
+        } else {
+            res.json(results);
+        }
+    });
+})
+router.post('/:table', (req, res) => {
+    if (isTableNotAuthorized(req.params.table)) {
+        res.send(400);
+        return;
+    }
+    connection.query('INSERT into ' + req.params.table + ' SET ?', req.body, (err, results) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Erreur lors de la sauvegarde d'un " + req.params.table);
+        }
+        else {
+            res.sendStatus(200);
+        }
+    })
+});
+router.put('/:table/:id', (req, res) => {
+    const idEmployee = req.params.id;
+    const formData = req.body;
+    console.log({formData});
+    formData.updated_at = new Date();
+    console.log({formData});
+    if (isTableNotAuthorized(req.params.table)) {
+        res.send(400);
+        return;
+    }
+    connection.query('UPDATE ' + req.params.table + ' SET ? WHERE id = ?', [formData, idEmployee], err => {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Erreur lors de la modification d'un " + req.params.table);
+        } else {
+            res.sendStatus(200);
+        }
+
+    })
+})
+router.delete('/:table/:id', (req, res) => {
+    if (isTableNotAuthorized(req.params.table)) {
+        res.send(400);
+        return;
+    }
+    connection.query('DELETE FROM ' + req.params.table +' WHERE id =?',req.params.id, err => {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Erreur lors de la modification d'un " + req.params.table);
+        } else {
+            res.sendStatus(200);
+        }
+
+    })
+})
+
+module.exports = router;
